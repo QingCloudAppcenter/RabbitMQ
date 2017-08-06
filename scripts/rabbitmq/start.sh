e633b05c575dbe20ba15fd7f90b0867f2e0021a1
pengkeboy <deanpeng@yunify.com>
2017-08-06 12:12:15 +0800

    update start scripts

--- scripts/rabbitmq/start.sh
+++ scripts/rabbitmq/start.sh
@@ -3,6 +3,10 @@ rabbitmqctl node_health_check >/dev/null  2>&1
 if [ $? -eq 0 ]; then
      exit 0
 fi
+cookie=$(cat /etc/cluster_info |grep cluster_id=|awk -F = '{print $2}')
+chmod 750 /var/lib/rabbitmq/.erlang.cookie
+echo $cookie >/var/lib/rabbitmq/.erlang.cookie
+chmod 400 /var/lib/rabbitmq/.erlang.cookie
 /opt/bin/rabbitmq-server-custom.sh stop
 /opt/bin/rabbitmq-server-custom.sh start
 if [ $? -eq 0 ]; then
