fc254d0cec9253c84340b6ff64be8ee4b54e36d6
shengpengwang <shengpengwang@yunify.com>
2020-05-07 23:02:22 +0800

    optimize reload func 2020050723

--- ansible/roles/node-rabbitmq/files/opt/app/bin/node/rabbitmq-node.sh
+++ ansible/roles/node-rabbitmq/files/opt/app/bin/node/rabbitmq-node.sh
@@ -3,10 +3,14 @@ EC_SCALE_OUT_ERR=240
 
 start() {
   log " startMQ start"
-  [[ "${HOSTNAME}" != "${DISC_NODES:2:10}" ]] && sleep ${SID} # left 5s for 1 disc node prepare tables
-  retry 5 2 0 _start
+  [[ "${HOSTNAME}" != "${DISC_NODES:2:10}" ]] && { # wait for first disc node prepare tables
+     while [[ ! $(rabbitmqctl --node rabbit@${DISC_NODES:2:10} -s  node_health_check) =~ "passed" ]]; do 
+      sleep 2; #the first node not ready now
+    done
+  } 
+  _start
   #retry 2 1 0 initNode
-  retry 3 5 0 addNode2Cluster
+  addNode2Cluster
   log " startMQ end"
 }
 
@@ -25,33 +29,62 @@ initNode() {
   log " initRabbitmq end"
 }
 
+stop() {
+  if [[ "${DELETINGHOST:-null}" =~ "${HOSTNAME}" ]]; then 
+    rabbitmqctl stop_app;
+    rabbitmqctl reset;
+  fi
+  _stop
+}
+
+isClusterChanged() {
+  # 1: true  0: false
+  if [[ -f "/opt/app/bin/envs/instance.info.1" ]]; then
+    cmp -s /opt/app/bin/envs/instance.info /opt/app/bin/envs/instance.info.1
+  else
+    return 0
+  fi
+}
+
+reload() {
+  isNodeInitialized || return 0
+
+  case ${1} in
+    rabbitmq-server)
+      if isClusterChanged; then
+        log "first start or cluster didn't changed"
+        _reload rabbitmq-server
+      else
+        log "add node ${ADDINGHOST:-null}, del node ${DELETINGHOST:-null}"
+      fi
+      ;;
+    caddy)
+      _reload caddy ;;
+    *) 
+      _reload $@ ;;
+  esac
+}
 
 scale_in() {
-  local clusterInfo
-  clusterInfo=$(rabbitmqctl cluster_status --formatter=json)
-  #allNodes=$(echo $clusterInfo | jq -j '[.nodes.disc[], .nodes.ram[]]')
-  #runningNodes=$(echo $clusterInfo | jq '.running_nodes')
-  local deleteNodes=$(echo $clusterInfo | jq -c '[(.nodes.disc[], .nodes.ram[]?)]-[(.running_nodes[])]')
-  local dn=$(echo $deleteNodes | jq '.[]')
-  for i in $dn
-  do
-    local node=$(echo $i | awk -F \" '{print $2}')
-    rabbitmqctl forget_cluster_node $node
-    sed -i -e "s/'rabbit@${node}'//g" -e "s/\[\,/\[/g" -e "s/\,\]/\]/g" -e "s/\,\,/\,/g"  /data/mnesia/rabbit@${HOSTNAME}/cluster_nodes.config
-  done
-  rabbitmqctl start_app
+  if isClusterChanged && [[ -n "${DELETINGHOST}" ]]; then
+    for i in ${DELETINGHOST}; do
+      rabbitmqctl forget_cluster_node ${i}
+    done
+  fi
 }
 
 scale_out() {
-  for i in $(curl -s metadata/self/adding-hosts | grep instance_id | awk '{print $2}'); do
-    local clusterInfo=$(rabbitmqctl -t 3 cluster_status -n rabbit@${i} --formatter=json | jq -j '[.nodes.disc[], .nodes.ram[]?]')
-    if [[ "$(rabbitmqctl -t 3 node_health_check -n rabbit@${i})" =~ "passed" ]] && [[ "${clusterInfo}" =~ "${HOSTNAME}" ]]; then
-      log "${i} was clustered successful in scale-out";
-    else
-      log "${i} was clustering failed in scale-out";
-      exit 240
-    fi
-  done
+  if isClusterChanged && [[ -n "${ADDINGHOST}" ]]; then
+    for i in ${ADDINGHOST}; do
+      local clusterInfo=$(rabbitmqctl -t 3 cluster_status -n rabbit@${i} --formatter=json | jq -j '[.nodes.disc[], .nodes.ram[]?]')
+      if [[ "$(rabbitmqctl -t 3 node_health_check -n rabbit@${i})" =~ "passed" ]] && [[ "${clusterInfo}" =~ "${HOSTNAME}" ]]; then
+        log "${i} was clustered successful in scale-out";
+      else
+        log "${i} was clustering failed in scale-out";
+        exit 240
+      fi
+    done
+  fi
 }
 
 measure() {
