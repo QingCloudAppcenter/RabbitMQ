711e829458229ff5d1cf800ad16da8e7f75f9fc8
zhangpeng <pengzhang@yunify.com>
2021-07-23 17:18:09 +0800

    Fix:merge proxy shell and revise app config

--- ansible/roles/node-proxy/files/opt/app/bin/node/proxy-node.sh
+++ ansible/roles/node-proxy/files/opt/app/bin/node/proxy-node.sh
@@ -1,11 +1,3 @@
-initNode() {
-  _initNode
-  mkdir -p /data/haproxy/logs
-  chown -R haproxy.haproxy /data/haproxy
-  mkdir -p /data/keepalived/logs
-  chown -R root.root /data/keepalived
-}
-
 checkSvc() {
   checkActive ${1%%/*} || {
     log "Service '$1' is inactive."
@@ -18,4 +10,32 @@ checkSvc() {
       return 0
     }
   done
+}
+
+stop() {
+  log "INFO: Application is asked to stop . "
+  _stop || (log "ERROR: services in Node ${MY_INSTANCE_ID} failed to stop  . " && return 1)
+  log "INFO: Application stopped successfully  . "
+}
+
+start() {
+  log "INFO: Application is asked to start . "
+  _start || (log "ERROR: services in Node ${MY_INSTANCE_ID} failed to start  . " && return 1)
+  log "INFO: Application started successfully  . "
+}
+
+initNode() {
+  log "INFO: Application is about to initialize . "
+  _initNode || ( log "ERROR: Application failed to initialize . " && return 1 )
+  mkdir -p /data/haproxy/logs
+  chown -R haproxy.haproxy /data/haproxy
+  mkdir -p /data/keepalived/logs
+  chown -R root.root /data/keepalived
+  log "INFO: Application initialization completed  . "
+}
+
+reload() {
+  log "INFO: Application is asked to reload  . "
+  _reload $@
+  log "INFO: Application reloaded completely . "
 }
