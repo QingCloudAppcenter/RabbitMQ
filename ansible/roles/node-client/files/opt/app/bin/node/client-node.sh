86b1843c1e6e6c99d82586545de230f3904d8d98
Yi Jing <djangoyi@yunify.com>
2025-08-08 16:36:44 +0800

    fix: default password of client

--- ansible/roles/node-client/files/opt/app/bin/node/client-node.sh
+++ ansible/roles/node-client/files/opt/app/bin/node/client-node.sh
@@ -1,5 +1,5 @@
 initNode() {
   _initNode
   echo 'root:rabbitmq123' | chpasswd
-  echo -e "client\nclient\n" | adduser client > /dev/nul 2>&1 || echo "client:client123" | chpasswd
+  echo -e "client\nclient\n" | adduser client > /dev/null 2>&1 && echo "client:rabbitmq123" | chpasswd
 }
